cba6b70efa09d624b593c417c74c7692af8c9506
Daniel Kis-Pal <daniel.kispal@instructure.com>
2026-05-20 14:29:33 +0200

    spec: add save-time sanitization spec for ExternalFeedEntry
    ExternalFeedEntry#message already has a sanitize_field
    declaration but had no spec verifying the callback fires
    on save. CNVS-73071 (Epic 1) acceptance criteria require
    every sanitize_field column to have a save-time spec.
    
    Reads the raw column value via reload[:message] to bypass
    any reader override that would otherwise mask a broken
    before-save callback.
    
    refs CNVS-73079
    flag=none
    
    test plan:
    - bin/rspec spec/models/external_feed_entry_spec.rb
    - Both examples should pass (strips script tags on
      create; strips disallowed attributes on update)
    
    Change-Id: If4e8be9ff1fa51fa2bd7b0a8310603f7616d8a26
    Reviewed-on: https://gerrit.instructure.com/c/canvas-lms/+/410312
    Reviewed-by: Derek Williams <derek.williams@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>
    Reviewed-by: Hassan Raafat <hassan.ibrahim@instructure.com>
    QA-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>
    Product-Review: Daniel Kis-Pal <daniel.kispal@instructure.com>

---
+++ spec/models/external_feed_entry_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+#
+# Copyright (C) 2026 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+describe ExternalFeedEntry do
+  describe "save-time sanitization of message" do
+    before do
+      course_factory
+      @feed = external_feed_model(context: @course)
+    end
+
+    # Reads the raw column value, bypassing any reader override that could
+    # mask a broken before-save sanitize_field callback.
+    def persisted_message(entry)
+      entry.reload[:message]
+    end
+
+    it "strips script tags on create" do
+      entry = @feed.external_feed_entries.create!(
+        message: "<p>safe</p><script>alert(1)</script>"
+      )
+      expect(persisted_message(entry)).not_to include("<script>")
+      expect(persisted_message(entry)).not_to include("alert(1)")
+      expect(persisted_message(entry)).to include("<p>safe</p>")
+    end
+
+    it "strips disallowed attributes on update" do
+      entry = @feed.external_feed_entries.create!(message: "<p>placeholder</p>")
+      entry.update!(message: '<p onmouseover="alert(1)">x</p>')
+      expect(persisted_message(entry)).not_to include("onmouseover")
+      expect(persisted_message(entry)).not_to include("alert(1)")
+    end
+  end
+end
